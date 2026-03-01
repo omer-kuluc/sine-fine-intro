39adc9ae30f0b64e965a7333e3bef7f210dea42a
omer-kuluc <omerkuluc96@gmail.com>
2026-03-01 21:15:32 +0300

    ana yapı oluşturuldu

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Intro from './components/Intro'
+
+function App() {
+
+  return (
+    <>
+      <Intro />
+    </>
+  )
+}
+
+export default App
